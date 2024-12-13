49434bfebe639ddca01c3fedf0f242938ab0a71b
unknown <Brandon@DESKTOP-8F3TLQJ.goose.local>
2024-12-13 00:50:36 -0500

    day 6 progress

--- day6/sol2.go
+++ day6/sol2.go
@@ -26,7 +26,7 @@ func sol2(lines []string) int {
 
 	}
 
-	ret_slice := solve_puzzle(puzzle_map, current_x, current_y, 0)
+	ret_slice := solve_puzzle2(puzzle_map, current_x, current_y, 0)
 
 	slices.SortFunc(ret_slice, matrix_sort)
 
@@ -35,6 +35,55 @@ func sol2(lines []string) int {
 	ret = detect_potential_loops(ret_slice)
 
 	return ret
+
+}
+
+func solve_puzzle2(puzzle_map map[int][]rune, current_x, current_y, num_rotations int) [][]int {
+
+	tmp_coords := [][]int{}
+
+	// Base case of out of puzzle
+	if current_x > len(puzzle_map)-1 || current_x < 0 || current_y > len(puzzle_map[current_y])-1 || current_y < 0 {
+		return tmp_coords
+	}
+
+	// Hit obsticle so rotate and try again
+	if puzzle_map[current_x][current_y] == '#' {
+
+		switch num_rotations % 4 { // Undo last move
+		case 0: // Going up
+			current_x += 1
+
+		case 1: // Going right
+			current_y -= 1
+
+		case 2: // Going down
+			current_x -= 1
+
+		default: // Going left
+			current_y += 1
+		}
+
+		return slices.Concat(tmp_coords, solve_puzzle2(puzzle_map, current_x, current_y, num_rotations+1))
+	}
+
+	tmp_coords = [][]int{{current_x, current_y}}
+
+	switch num_rotations % 4 {
+	case 0: // Going up
+		tmp_coords = slices.Concat(tmp_coords, solve_puzzle2(puzzle_map, current_x-1, current_y, num_rotations))
+
+	case 1: // Going right
+		tmp_coords = slices.Concat(tmp_coords, solve_puzzle2(puzzle_map, current_x, current_y+1, num_rotations))
+
+	case 2: // Going down
+		tmp_coords = slices.Concat(tmp_coords, solve_puzzle2(puzzle_map, current_x+1, current_y, num_rotations))
+
+	default: // Going left
+		tmp_coords = slices.Concat(tmp_coords, solve_puzzle2(puzzle_map, current_x, current_y-1, num_rotations))
+	}
+
+	return tmp_coords
 
 }
 
